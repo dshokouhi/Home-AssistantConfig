82653d8fbecea1887f23e6edeedff20cc6322395
dshokouhi <dshokouhi@gmail.com>
2018-05-31 14:34:04 -0700

    add some arduino code and some custom components alarm control panel is untested at the moment but zha sensor is with battery readings for centralite devices

---
+++ arduino/mqtt-smoker-probe-template/mqtt-smoker-probe-template.ino
@@ -0,0 +1,236 @@
+// Many thanks to Aderusha for the following project where 99% of this code comes from: https://github.com/aderusha/MQTTCarPresence
+// In fact you need to follow the steps at that URL to get started here so do that first (including how to upload the code).  Make sure you install the MAX6675 library as well in the Adruino IDE.
+// 
+// Parts needed:
+// ESP8266 NodeMCUv2, MAX6675 Chip, K-type thermocouple, 5 female to female dupont cables, powerbank
+
+// MAX6675 pins     ESP8266 pins
+// GND              GND
+// VCC              3.3V
+// SCK              GPIO12
+// CS               GPIO13
+// SO               GPIO15
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Modify these values for your environment
+const char* wifiSSID = "";  // Your WiFi network name
+const char* wifiPassword = "";  // Your WiFi network password
+const char* otaPassword = "";  // OTA update password
+const char* mqttServer = "";  // Your MQTT server IP address
+const char* mqttUser = ""; // mqtt username, set to "" for no user
+const char* mqttPassword = ""; // mqtt password, set to "" for no password
+const String mqttNode = "BBQ"; // Your unique hostname for this device
+const String mqttDiscoveryPrefix = "homeassistant"; // Home Assistant MQTT Discovery, see https://home-assistant.io/docs/mqtt/discovery/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Home Assistant MQTT Discovery, see https://home-assistant.io/docs/mqtt/discovery/
+// We'll create one binary_sensor device to track MQTT connectivity
+const String mqttDiscoBinaryStateTopic = mqttDiscoveryPrefix + "/binary_sensor/" + mqttNode + "/state";
+const String mqttDiscoBinaryConfigTopic = mqttDiscoveryPrefix + "/binary_sensor/" + mqttNode + "/config";
+// And a sensor for WiFi signal strength
+const String mqttDiscoTempStateTopic = mqttDiscoveryPrefix + "/sensor/" + mqttNode + "-temperature/state";
+const String mqttDiscoTempConfigTopic = mqttDiscoveryPrefix + "/sensor/" + mqttNode + "-temperature/config";
+// And a sensor for device uptime
+//const String mqttDiscoVoltStateTopic = mqttDiscoveryPrefix + "/sensor/" + mqttNode + "-voltage/state";
+//const String mqttDiscoVoltConfigTopic = mqttDiscoveryPrefix + "/sensor/" + mqttNode + "-voltage/config";
+
+// The strings below will spill over the PubSubClient_MAX_PACKET_SIZE 128
+// You'll need to manually set MQTT_MAX_PACKET_SIZE in PubSubClient.h to 512
+const String mqttDiscoBinaryConfigPayload = "{\"name\": \"" + mqttNode + "\", \"device_class\": \"connectivity\", \"state_topic\": \"" + mqttDiscoBinaryStateTopic + "\"}";
+const String mqttDiscoTempConfigPayload = "{\"name\": \"" + mqttNode + "-temperature\", \"state_topic\": \"" + mqttDiscoTempStateTopic + "\", \"unit_of_measurement\": \"F\", \"value_template\": \"{{ value }}\"}";
+//const String mqttDiscoVoltConfigPayload = "{\"name\": \"" + mqttNode + "-voltage\", \"state_topic\": \"" + mqttDiscoVoltStateTopic + "\", \"unit_of_measurement\": \"v\", \"value_template\": \"{{ value }}\"}";
+
+// Set the signal strength and uptime reporting interval in milliseconds
+const unsigned long reportInterval = 5000;
+unsigned long reportTimer = millis();
+
+// Set LED "twinkle" time for maximum daylight visibility
+const unsigned long twinkleInterval = 50;
+unsigned long twinkleTimer = millis();
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <PubSubClient.h>
+#include "max6675.h"
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+/*************************************************/
+/* Settings for MAX6675 Measurements             */
+/*************************************************/
+MAX6675 thermocouple(D6, D7, D8);
+String temperature;  // Values read from sensor
+
+/*************************************************/
+/* Settings for VCC Measurements                 */
+/*************************************************/
+//ADC_MODE(ADC_VCC);
+//float espVoltage;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// System setup
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.begin(115200);
+
+  Serial.println("\nHardware initialized, starting program load");
+
+  // Start up networking
+  setupWifi();
+
+  // Create server and assign callbacks for MQTT
+  mqttClient.setServer(mqttServer, 1883);
+  mqttClient.setCallback(mqtt_callback);
+
+  // Start up OTA
+  if (otaPassword[0]) {
+    setupOTA();
+  }
+
+  Serial.println("Initialization complete\n");
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Main execution loop
+void loop() {
+  // check WiFi connection
+  if (WiFi.status() != WL_CONNECTED) {
+    setupWifi();
+  }
+
+  // check MQTT connection
+  if (!mqttClient.connected()) {
+    mqttConnect();
+  }
+
+  // MQTT client loop
+  if (mqttClient.connected()) {
+    mqttClient.loop();
+  }
+
+  // LED twinkle
+  if (mqttClient.connected() && ((millis() - twinkleTimer) >= twinkleInterval)) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    twinkleTimer = millis();
+  }
+
+  // Report signal strength and uptime
+  if (mqttClient.connected() && ((millis() - reportTimer) >= reportInterval)) {
+    //String signalStrength = String(WiFi.RSSI());
+    //String uptimeTimer = String(millis());
+
+     delay(6000);
+      temperature = String(thermocouple.readFahrenheit());
+      yield();
+      // Determine the VCC Voltage
+      //espVoltage = ESP.getVcc();
+      //yield();
+    mqttClient.publish(mqttDiscoTempStateTopic.c_str(), temperature.c_str());
+    //mqttClient.publish(mqttDiscoVoltStateTopic, espVoltage);
+    reportTimer = millis();
+  }
+
+  // OTA loop
+  if (otaPassword[0]) {
+    ArduinoOTA.handle();
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Functions
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Handle incoming commands from MQTT
+void mqtt_callback(char* topic, byte* payload, unsigned int payloadLength) {
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Connect to WiFi
+void setupWifi() {
+  Serial.print("Connecting to WiFi network: " + String(wifiSSID));
+  WiFi.hostname(mqttNode.c_str());
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifiSSID, wifiPassword);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    // Wait 500msec seconds before retrying
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected successfully and assigned IP: " + WiFi.localIP().toString());
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// MQTT connection and subscriptions
+void mqttConnect() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("Attempting MQTT connection to broker: " + String(mqttServer));
+  // Attempt to connect to broker, setting last will and testament
+  if (mqttClient.connect(mqttNode.c_str(), mqttUser, mqttPassword, mqttDiscoBinaryStateTopic.c_str(), 1, 1, "OFF")) {
+    // when connected, record signal strength and reset reporting timer
+    //String signalStrength = String(WiFi.RSSI());
+    reportTimer = millis();
+    //String uptimeTimer = String(millis());
+
+     //delay(6000);
+      temperature = String(thermocouple.readFahrenheit());
+      yield();
+      // Determine the VCC Voltage
+      //espVoltage = ESP.getVcc();
+     // yield();
+    
+    // publish MQTT discovery topics and device state
+    Serial.println("MQTT discovery connectivity config: [" + mqttDiscoBinaryConfigTopic + "] : [" + mqttDiscoBinaryConfigPayload + "]");
+    Serial.println("MQTT discovery connectivity state: [" + mqttDiscoBinaryStateTopic + "] : [ON]");
+    Serial.println("MQTT discovery temperature config: [" + mqttDiscoTempConfigTopic + "] : [" + mqttDiscoTempConfigPayload + "]");
+    Serial.println("MQTT discovery temperature state: [" + mqttDiscoTempStateTopic + "] : " + temperature);
+    //Serial.println("MQTT discovery voltage config: [" + mqttDiscoVoltConfigTopic + "] : [" + mqttDiscoVoltConfigPayload + "]");
+    //Serial.println("MQTT discovery voltage state: [" + mqttDiscoVoltStateTopic + "] : " + espVoltage);
+    mqttClient.publish(mqttDiscoBinaryConfigTopic.c_str(), mqttDiscoBinaryConfigPayload.c_str(), true);
+    mqttClient.publish(mqttDiscoBinaryStateTopic.c_str(), "ON");
+    mqttClient.publish(mqttDiscoTempConfigTopic.c_str(), mqttDiscoTempConfigPayload.c_str(), true);
+    mqttClient.publish(mqttDiscoTempStateTopic.c_str(), temperature.c_str());
+   // mqttClient.publish(mqttDiscoVoltConfigTopic.c_str(), mqttDiscoVoltConfigPayload.c_str(), true);
+   // mqttClient.publish(mqttDiscoVoltStateTopic.c_str(), espVoltage);
+    Serial.println("MQTT connected");
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else {
+    Serial.println("MQTT connection failed, rc=" + String(mqttClient.state()));
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// (mostly) boilerplate OTA setup from library examples
+void setupOTA() {
+  // Start up OTA
+  // ArduinoOTA.setPort(8266); // Port defaults to 8266
+  ArduinoOTA.setHostname(mqttNode.c_str());
+  ArduinoOTA.setPassword(otaPassword);
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("ESP OTA:  update start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("ESP OTA:  update complete");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    //Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.println("ESP OTA:  ERROR code " + String(error));
+    if (error == OTA_AUTH_ERROR) Serial.println("ESP OTA:  ERROR - Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("ESP OTA:  ERROR - Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("ESP OTA:  ERROR - Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("ESP OTA:  ERROR - Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("ESP OTA:  ERROR - End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("ESP OTA:  Over the Air firmware update ready");
+}
---
+++ custom_components/sensor/zha.py
@@ -0,0 +1,295 @@
+"""
+Sensors on Zigbee Home Automation networks.
+
+For more details on this platform, please refer to the documentation
+at https://home-assistant.io/components/sensor.zha/
+"""
+import asyncio
+import logging
+
+from homeassistant.components.sensor import DOMAIN
+from homeassistant.components import zha
+from homeassistant.const import TEMP_CELSIUS
+from homeassistant.util.temperature import convert as convert_temperature
+
+_LOGGER = logging.getLogger(__name__)
+
+DEPENDENCIES = ['zha']
+
+
+@asyncio.coroutine
+def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
+    """Set up Zigbee Home Automation sensors."""
+    discovery_info = zha.get_discovery_info(hass, discovery_info)
+    if discovery_info is None:
+        return
+
+    sensor = yield from make_sensor(discovery_info)
+    async_add_devices([sensor], update_before_add=True)
+
+
+@asyncio.coroutine
+def make_sensor(discovery_info):
+    """Create ZHA sensors factory."""
+    from zigpy.zcl.clusters.measurement import (
+        RelativeHumidity, TemperatureMeasurement, IlluminanceMeasurement
+    )
+    from zigpy.zcl.clusters.general import PowerConfiguration
+    from zigpy.zcl.clusters.smartenergy import Metering
+    from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
+    in_clusters = discovery_info['in_clusters']
+    if RelativeHumidity.cluster_id in in_clusters:
+        sensor = RelativeHumiditySensor(**discovery_info)
+    elif TemperatureMeasurement.cluster_id in in_clusters:
+        sensor = TemperatureSensor(**discovery_info)
+    elif PowerConfiguration.cluster_id in in_clusters \
+            and discovery_info['manufacturer'] == 'CentraLite':
+        sensor = CentraliteBatterySensor(**discovery_info)
+    elif Metering.cluster_id in in_clusters:
+        sensor = MeteringSensor(**discovery_info)
+    elif IlluminanceMeasurement.cluster_id in in_clusters:
+        sensor = IlluminanceMeasurementSensor(**discovery_info)
+    elif ElectricalMeasurement.cluster_id in in_clusters:
+        sensor = ElectricalMeasurementSensor(**discovery_info)
+        return sensor
+    else:
+        sensor = Sensor(**discovery_info)
+
+    if discovery_info['new_join']:
+        cluster = list(in_clusters.values())[0]
+        yield from cluster.bind()
+        yield from cluster.configure_reporting(
+            sensor.value_attribute, 300, 600, sensor.min_reportable_change,
+        )
+
+    return sensor
+
+
+class Sensor(zha.Entity):
+    """Base ZHA sensor."""
+
+    _domain = DOMAIN
+    value_attribute = 0
+    min_reportable_change = 1
+
+    @property
+    def state(self) -> str:
+        """Return the state of the entity."""
+        if isinstance(self._state, float):
+            return str(round(self._state, 2))
+        return self._state
+
+    @property
+    def should_poll(self) -> bool:
+        """Return True if entity has to be polled for state.
+
+        False if entity pushes its state to HA.
+        """
+        return False
+
+    def attribute_updated(self, attribute, value):
+        """Handle attribute update from device."""
+        _LOGGER.debug("Attribute updated: %s %s %s", self, attribute, value)
+        if attribute == self.value_attribute:
+            self._state = value
+            self.async_schedule_update_ha_state()
+
+    async def async_update(self):
+        """Retrieve latest state."""
+        result = await zha.safe_read(
+            list(self._in_clusters.values())[0],
+            [self.value_attribute]
+        )
+        self._state = result.get(self.value_attribute, self._state)
+
+class TemperatureSensor(Sensor):
+    """ZHA temperature sensor."""
+
+    min_reportable_change = 50  # 0.5'C
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity."""
+        return self.hass.config.units.temperature_unit
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        if self._state == 'unknown':
+            return 'unknown'
+        celsius = round(float(self._state) / 100, 1)
+        return round(convert_temperature(celsius, TEMP_CELSIUS,
+                                         self.unit_of_measurement), 1)
+
+
+class RelativeHumiditySensor(Sensor):
+    """ZHA relative humidity sensor."""
+
+    min_reportable_change = 50  # 0.5%
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity."""
+        return '%'
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        if self._state == 'unknown':
+            return 'unknown'
+
+        return round(float(self._state) / 100, 1)
+
+
+class GenericBatterySensor(Sensor):
+    """ZHA generic battery sensor."""
+
+    value_attribute = 32
+    battery_sizes = {
+        0: 'No battery',
+        1: 'Built in',
+        2: 'Other',
+        3: 'AA',
+        4: 'AAA',
+        5: 'C',
+        6: 'D',
+        7: 'CR2',
+        8: 'CR123A',
+        255: 'Unknown'
+    }
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity."""
+        return '%'
+
+    @asyncio.coroutine
+    def async_update(self):
+        """Retrieve latest state."""
+        _LOGGER.debug("%s async_update", self.entity_id)
+
+        result = yield from zha.safe_read(
+            self._endpoint.power,
+            ['battery_size', 'battery_quantity', 'battery_voltage']
+        )
+        self._device_state_attributes['battery_size'] = \
+            self.battery_sizes.get(
+                result.get('battery_size', 255),
+                'Unknown'
+            )
+        self._device_state_attributes['battery_quantity'] = result.get(
+            'battery_quantity', 'Unknown')
+        self._state = result.get('battery_voltage', self._state)
+
+
+class CentraliteBatterySensor(GenericBatterySensor):
+    """ZHA battery sensor."""
+
+    # currently restricted to centralite sensors because the value
+    # conversion is specific to centralite sensors.
+
+    minVolts = 15
+    maxVolts = 28
+    values = {
+        28: 100,
+        27: 100,
+        26: 100,
+        25: 90,
+        24: 90,
+        23: 70,
+        22: 70,
+        21: 50,
+        20: 50,
+        19: 30,
+        18: 30,
+        17: 15,
+        16: 1,
+        15: 0
+    }
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        if self._state == 'unknown':
+            return 'unknown'
+
+        if self._state < self.minVolts:
+            self._state = self.minVolts
+        elif self._state > self.maxVolts:
+            self._state = self.maxVolts
+
+        return self.values.get(self._state, 'unknown')
+
+
+class MeteringSensor(Sensor):
+    """ZHA Metering sensor."""
+
+    value_attribute = 1024
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity."""
+        return 'W'
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        if self._state is None:
+            return None
+
+        return round(float(self._state))
+
+
+class ElectricalMeasurementSensor(Sensor):
+    """ZHA Electrical Measurement sensor."""
+
+    value_attribute = 1291
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity."""
+        return 'W'
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        if self._state is None:
+            return None
+
+        return round(float(self._state) / 10, 1)
+
+    @property
+    def should_poll(self) -> bool:
+        """Return True if entity has to be polled for state.
+
+        False if entity pushes its state to HA.
+        """
+        return True
+
+    #@asyncio.coroutine
+    async def async_update(self):
+        """Retrieve latest state."""
+        _LOGGER.debug("%s async_update", self.entity_id)
+
+        result = await zha.safe_read(
+            self._endpoint.electrical_measurement,
+			['active_power'],
+			allow_cache=False)
+        self._state = result.get('active_power', self._state)
+
+
+class IlluminanceMeasurementSensor(Sensor):
+    """ZHA lux sensor."""
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity."""
+        return 'lux'
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        if self._state == 'unknown':
+            return 'unknown'
+
+        return self._state
